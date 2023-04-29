7dbb90f6c1d658406a32889a5669793c019089f9
Jihun <131355484+Jihun-s@users.noreply.github.com>
2023-04-29 16:22:37 +0900

    [unrated] Title: 대문자로 바꾸기, Time: 0.12 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String myString) {
+        String answer = "";
+        if(myString.length()>=1&&myString.length()<=100000)
+            if(myString.matches("[a-zA-Z]+"))
+            {
+                answer = myString.toUpperCase();
+            }
+        return answer;
+    }
+}
