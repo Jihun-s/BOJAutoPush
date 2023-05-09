e38ddc23800963ad4be6c6703512878180e88256
Jihun <131355484+Jihun-s@users.noreply.github.com>
2023-05-09 17:53:02 +0900

    [unrated] Title: rny_string, Time: 1.61 ms, Memory: 69.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181863. rny＿string/rny＿string.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String solution(String rny_string) {
+        String answer = "";
+        for(int i = 0; i<rny_string.length(); i++)
+        {
+            if(rny_string.substring(i,i+1).equals("m"))
+            {
+                answer += "rn";
+            }else
+            {
+                answer += Character.toString(rny_string.charAt(i));
+            }
+        }
+        return answer;
+    }
+}
