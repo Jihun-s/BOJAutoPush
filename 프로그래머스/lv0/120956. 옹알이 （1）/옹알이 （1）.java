0b08d8c3c22bbe6c5297a510884369685e812e33
Jihun <131355484+Jihun-s@users.noreply.github.com>
2023-05-23 11:56:15 +0900

    [level 0] Title: 옹알이 (1), Time: 0.19 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
+++ 프로그래머스/lv0/120956. 옹알이 （1）/옹알이 （1）.java
@@ -1,21 +1,31 @@
 class Solution {
     public int solution(String[] babbling) {
         int answer = 0;
+        String[] bab = new String[babbling.length];
+        int i = 0;
         for(String s:babbling)
         {
             if(s.contains("aya"))
             {
-                s = s.replace("aya", "");
+                s = s.replace("aya", "1");
             }if(s.contains("ye"))
             {
-                s = s.replace("ye", "");
+                s = s.replace("ye", "1");
             }if(s.contains("woo"))
             {
-                s = s.replace("woo", "");
+                s = s.replace("woo", "1");
             }if(s.contains("ma"))
             {
-                s = s.replace("ma", "");
-            }if(s.equals("")){
+                s = s.replace("ma", "1");
+            }
+            bab[i] = s;
+            i++;
+        }
+        for(String s:bab)
+        {
+            if(s.equals("1")||s.equals("11")||s.equals("111")
+               ||s.equals("1111"))
+            {
                 answer++;
             }
         }
