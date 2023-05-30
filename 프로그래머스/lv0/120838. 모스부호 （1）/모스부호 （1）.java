b9bcc558a255ace2ae71fc01f26a11057a621c6b
Jihun <131355484+Jihun-s@users.noreply.github.com>
2023-05-30 10:08:49 +0900

    [level 0] Title: 모스부호 (1), Time: 1.39 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.java
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -1,6 +1,20 @@
+import java.util.HashMap;
 class Solution {
     public String solution(String letter) {
         String answer = "";
+        HashMap<String, String> morseToAlphabet = new HashMap<>();
+        String[] morseCodes = {".-","-...","-.-.","-..",".","..-.",
+        "--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",
+        ".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+        String[] alphabets = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j",             "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y",         "z"};
+        for (int i = 0; i < morseCodes.length; i++) {
+            morseToAlphabet.put(morseCodes[i], alphabets[i]);
+        }
+        String[] letter1 = letter.split(" ");
+        for(String s:letter1)
+        {
+            answer += morseToAlphabet.get(s);
+        }
         return answer;
     }
 }
