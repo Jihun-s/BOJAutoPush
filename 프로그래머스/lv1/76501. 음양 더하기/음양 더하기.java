da1eeec497f314d31a0789360f97a283a070c20c
Jihun <131355484+Jihun-s@users.noreply.github.com>
2023-07-06 13:11:48 +0900

    [level 1] Title: 음양 더하기, Time: 0.07 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.java
@@ -2,10 +2,7 @@ class Solution {
     public int solution(int[] absolutes, boolean[] signs) {
         int answer = 0;
         for(int i = 0; i<signs.length; i++){
-            int k = 1;
-            if(signs[i]){
-            }else k = -1;
-            answer += absolutes[i] * k;
+            answer += absolutes[i] * (signs[i]? 1:-1);
         }
         return answer;
     }
