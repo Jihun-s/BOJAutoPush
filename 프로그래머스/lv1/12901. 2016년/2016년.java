8ced406f88e0e339d4d88ded47b49a0fcd445365
Jihun <131355484+Jihun-s@users.noreply.github.com>
2023-08-31 08:26:29 +0900

    [level 1] Title: 2016년, Time: 18.03 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+class Solution {
+    public String solution(int a, int b) {
+        return LocalDate.of(2016, a, b).getDayOfWeek().getDisplayName(TextStyle.SHORT, Locale.getDefault()).toUpperCase();
+    }
+}
